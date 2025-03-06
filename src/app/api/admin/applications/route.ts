c2432242f77497f0dee9cc04b5fe8525a00800e5
Usama Irshad <itsusamairshad@gmail.com>
2025-03-06 07:29:22 +0500

    update

--- src/app/api/admin/applications/route.ts
+++ src/app/api/admin/applications/route.ts
@@ -3,31 +3,12 @@ import { cookies } from 'next/headers';
 import jwt from 'jsonwebtoken';
 import prisma from '@/lib/prisma';
 
-// ✅ Verify Admin Middleware
-export async function verifyAdmin() {
-  const token = (await cookies()).get("token")?.value;
-  if (!token) {
-    return { error: 'Unauthorized', status: 401 };
-  }
-
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET!) as { role: string };
-    if (decoded.role !== 'ADMIN') {
-      return { error: 'Only admins can perform this action', status: 403 };
-    }
-    return { decoded };
-  } catch (error) {
-    return { error: 'Invalid token', status: 401 };
-  }
-}
 
 // ✅ Fetch All Applications (GET /api/admin/applications)
 export async function GET() {
   // **Admin verification**
-  const { error, status, decoded } = await verifyAdmin();
-  if (error) {
-    return NextResponse.json({ error }, { status });
-  }
+  
+  
 
   try {
     // Fetch all applications with user and job details
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,20 @@
 
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+// import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -30,7 +30,7 @@ export default function RootLayout({
     <html lang="en">
     
       <body
-      className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      // className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
           <div className=" min-h-screen flex flex-col">
            
